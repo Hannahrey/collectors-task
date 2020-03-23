f0421ab5f19cf5255740c931fab6c5f7459315e4
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-03-23 15:53:32 +0000

    amends to flex boxes

--- index.php
+++ index.php
@@ -66,6 +66,28 @@
 								<li>Served: Crushed ice</li>
 							</ul>
 					</div>
+					<div class="box1">
+							<h2>Mojito</h2>
+							<ul>
+								<li>Alcohol Base: Rum </li>
+								<li>Taste Profile: Refreshing</li>
+								<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
+								<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
+								<li>Strength: Medium</li>
+								<li>Served: Crushed ice</li>
+							</ul>
+					</div>
+					<div class="box1">
+							<h2>Mojito</h2>
+							<ul>
+								<li>Alcohol Base: Rum </li>
+								<li>Taste Profile: Refreshing</li>
+								<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
+								<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
+								<li>Strength: Medium</li>
+								<li>Served: Crushed ice</li>
+							</ul>
+					</div>
 
 					
 				</div>
