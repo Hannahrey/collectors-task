1865a6ff2a9bcaa9d1e02f1d707d31653fa0610a
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-03-23 16:20:10 +0000

    tidying up the code

--- index.php
+++ index.php
@@ -1,103 +1,101 @@
 <html lang="en-GB">
-<head>
-	<title>Cocktails</title>
-	<link rel="stylesheet" type="text/css" href="normalize.css" />
-	<link rel="stylesheet" type="text/css" href="style.css" />
-	<link href="https://fonts.googleapis.com/css?family=Mrs+Sheppards|Sen&display=swap" rel="stylesheet">
-</head>
+	<head>
+		<title>Cocktails</title>
+		<link rel="stylesheet" type="text/css" href="normalize.css" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<link href="https://fonts.googleapis.com/css?family=Mrs+Sheppards|Sen&display=swap" rel="stylesheet">
+	</head>
 
-<body>
-	<!-- top banner -->
-	<header>
-		<div class="container_top">
-            <h1>Cocktails</h1>
-
+	<body>
+		<!-- top banner -->
+		<header>
+			<div class="container_top">
+				<h1>Cocktails</h1>
 			</div>
-		</div>
-	</header>
+		</header>
 
-	<div class="container_main">
-		<div class="container_link">
-		<a href="./add_item.php">Add new cocktail</a>
-		</div>
+		<div class="container_main">
+			<div class="container_link">
+			<a href="./add_item.php">Add new cocktail</a>
+			</div>
 
-		<div class="cocktails_box">
+			<div class="cocktails_box">
 				<div class="box1">
-							<h2>Mojito</h2>
-							<ul>
-								<li>Alcohol Base: Rum </li>
-								<li>Taste Profile: Refreshing</li>
-								<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
-								<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
-								<li>Strength: Medium</li>
-								<li>Served: Crushed ice</li>
-							</ul>
-					</div>
-					<div class="box1">
-							<h2>Tequila Sunrise</h2>
-							<ul>
-								<li>Alcohol Base: Rum </li>
-								<li>Taste Profile: Refreshing</li>
-								<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
-								<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
-								<li>Strength: Medium</li>
-								<li>Served: Crushed ice</li>
-							</ul>
-					</div>
-					<div class="box1">
-							<h2>Mojito</h2>
-							<ul>
-								<li>Alcohol Base: Rum </li>
-								<li>Taste Profile: Refreshing</li>
-								<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
-								<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
-								<li>Strength: Medium</li>
-								<li>Served: Crushed ice</li>
-							</ul>
-					</div>
-					<div class="box1">
-							<h2>Mojito</h2>
-							<ul>
-								<li>Alcohol Base: Rum </li>
-								<li>Taste Profile: Refreshing</li>
-								<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
-								<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
-								<li>Strength: Medium</li>
-								<li>Served: Crushed ice</li>
-							</ul>
-					</div>
-					<div class="box1">
-							<h2>Mojito</h2>
-							<ul>
-								<li>Alcohol Base: Rum </li>
-								<li>Taste Profile: Refreshing</li>
-								<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
-								<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
-								<li>Strength: Medium</li>
-								<li>Served: Crushed ice</li>
-							</ul>
-					</div>
-					<div class="box1">
-							<h2>Mojito</h2>
-							<ul>
-								<li>Alcohol Base: Rum </li>
-								<li>Taste Profile: Refreshing</li>
-								<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
-								<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
-								<li>Strength: Medium</li>
-								<li>Served: Crushed ice</li>
-							</ul>
-					</div>
-
-					
+					<h2>Mojito</h2>
+					<ul>
+						<li>Alcohol Base: Rum </li>
+						<li>Taste Profile: Refreshing</li>
+						<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
+						<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
+						<li>Strength: Medium</li>
+						<li>Served: Crushed ice</li>
+					</ul>	
 				</div>
-				
-	
-	
-	</div>
+				<div class="box1">
+					<h2>Tequila Sunrise</h2>
+					<ul>
+						<li>Alcohol Base: Rum </li>
+						<li>Taste Profile: Refreshing</li>
+						<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
+						<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
+						<li>Strength: Medium</li>
+						<li>Served: Crushed ice</li>
+					</ul>
+				</div>
+				<div class="box1">
+					<h2>Mojito</h2>
+					<ul>
+						<li>Alcohol Base: Rum </li>
+						<li>Taste Profile: Refreshing</li>
+						<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
+						<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
+						<li>Strength: Medium</li>
+						<li>Served: Crushed ice</li>
+					</ul>
+				</div>
+				<div class="box1">
+					<h2>Mojito</h2>
+					<ul>
+						<li>Alcohol Base: Rum </li>
+						<li>Taste Profile: Refreshing</li>
+						<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
+						<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
+						<li>Strength: Medium</li>
+						<li>Served: Crushed ice</li>
+					</ul>
+				</div>
+				<div class="box1">
+					<h2>Mojito</h2>
+					<ul>
+						<li>Alcohol Base: Rum </li>
+						<li>Taste Profile: Refreshing</li>
+						<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
+						<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
+						<li>Strength: Medium</li>
+						<li>Served: Crushed ice</li>
+					</ul>
+				</div>
+				<div class="box1">
+					<h2>Mojito</h2>
+					<ul>
+						<li>Alcohol Base: Rum </li>
+						<li>Taste Profile: Refreshing</li>
+						<li>Ingredients: 4 cl white rum, 3 cl fresh lime juice, 6 sprigs of mint, 2 teaspoons sugar (or 2 cl of sugar syrup), soda water.</li>
+						<li>Method: Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice. Serve with straw. </li>
+						<li>Strength: Medium</li>
+						<li>Served: Crushed ice</li>
+					</ul>
+				</div>
+
+						
+			</div>
+					
+		
+		
+		</div>
 
 
-</body>
+	</body>
 </html>
 
 <?php
