537ed33026aa56ac63f03f9c76669402526e7eed
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-03-23 14:35:27 +0000

    starting on the front end

---
+++ index.php
@@ -0,0 +1,52 @@
+<html lang="en-GB">
+<head>
+	<title>Cocktails</title>
+	<link rel="stylesheet" type="text/css" href="normalize.css" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link href="https://fonts.googleapis.com/css?family=Mrs+Sheppards|Sen&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<!-- top banner -->
+	<header>
+		<div class="container_top">
+            <h1>Cocktails</h1>
+
+			</div>
+		</div>
+	</header>
+
+	<div class="container_main">
+
+	<div class="cocktails_box">
+			<div class="box1">
+						<div class="name">Mayden Academy Logo</div>
+						<p>Take a look</p>
+				</div>
+
+				<div class="box1">
+						<div class="name">Mayden Academy Logo</div>
+						<p>Take a look</p>
+				</div>	
+				<div class="box1">
+						<div class="name">Mayden Academy Logo</div>
+						<p>Take a look</p>
+				</div>
+				<div class="box1">
+						<div class="name">Mayden Academy Logo</div>
+						<p>Take a look</p>
+				</div>
+			</div>
+			
+	
+	
+	</div>
+
+
+</body>
+</html>
+
+<?php
+
+
+?>
