a7483264f4d7bcba5375fd312f59926e820f669f
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-03-25 14:09:13 +0000

    get DB function

---
+++ getDB_function.php
@@ -0,0 +1,19 @@
+
+<?php
+// to link to the DB
+
+$servername = "db";
+
+$username = "root";
+
+$password = "password";
+
+$dbname = "Cocktails";
+
+function getDb(string $servername, string $dbname, string $username, string $password) : PDO {
+
+    $db = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $db;
+};
+
